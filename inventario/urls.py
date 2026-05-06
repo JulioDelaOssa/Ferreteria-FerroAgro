1521f9e2c36f0548a0c28f3959cae1db9af6927c
JulioDelaOssa <juliodlaossam2@gmail.com>
2026-05-06 18:59:27 -0500

    Agregar edicion de productos

--- inventario/urls.py
+++ inventario/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.home, name='dashboard'),
     path('productos/', views.producto_lista, name='producto_lista'),
     path('productos/nuevo/', views.producto_crear, name='producto_crear'),
+    path('productos/editar/<int:producto_id>/', views.producto_editar, name='producto_editar'),
+
 
     path(
         'login/',
--- inventario/views.py
+++ inventario/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 
 from .forms import ProductoForm
 from .models import Producto
+
+
 
 @login_required
 def home(request):
@@ -30,4 +32,22 @@ def producto_crear(request):
     return render(request, 'inventario/producto_form.html', {
         'form': form,
         'titulo': 'Nuevo producto'
+    })
+
+@login_required
+def producto_editar(request, producto_id):
+    producto = get_object_or_404(Producto, id=producto_id)
+
+    if request.method == 'POST':
+        form = ProductoForm(request.POST, instance=producto)
+
+        if form.is_valid():
+            form.save()
+            return redirect('producto_lista')
+    else:
+        form = ProductoForm(instance=producto)
+
+    return render(request, 'inventario/producto_form.html', {
+        'form': form,
+        'titulo': 'Editar producto'
     })
